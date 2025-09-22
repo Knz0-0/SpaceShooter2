4e6c1ad7d96351ff5eb327a46fe9c5db3a9cdc88
Kenzo Rufflet--Ejiri <ejirikenzo@gmail.com>
2025-09-22 10:15:19 -0400

    First commit

---
+++ Source/TP1_SpaceShooter2/TP1_SpaceShooter2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TP1_SpaceShooter2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP1_SpaceShooter2, "TP1_SpaceShooter2" );
